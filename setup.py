c8899e182d190374f8659f22e6e041a889cc6fcd
connor-philip <connorphilip12@hotmail.com>
2018-02-14 20:38:29 +0000

    Adding setup py for create_tests

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name="create_tests",
+      description="create_tests for lazy unit tester",
+      author="Connor Philip",
+      author_email="connorphilip12@hotmail.com",
+      py_modules=["create_tests"],
+      )
