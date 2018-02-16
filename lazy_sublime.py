8e156d8343b3ee181c35a2930163096686851b59
connor-philip <connorphilip12@hotmail.com>
2018-02-16 00:15:50 +0000

    changing command name

--- lazy_sublime.py
+++ lazy_sublime.py
@@ -3,7 +3,7 @@ import sublime_plugin
 from lazy_unit_tester.create_tests import CreateTests
 
 
-class LazyCommand(sublime_plugin.TextCommand):
+class CreatePythonUnittestsCommand(sublime_plugin.TextCommand):
     def run(self, view):
         fileName = self.view.file_name()
         instance = CreateTests(fileName, False, False)
