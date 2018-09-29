cc8b0f0c34ef428c52d22d03f541b1bf3ece2a40
connor-philip <connorphilip12@hotmail.com>
2018-09-30 00:05:27 +0100

    Adding cli option to define test directory

--- lazy.py
+++ lazy.py
@@ -4,11 +4,12 @@ import sys
 
 parser = argparse.ArgumentParser(prog="command")
 parser.add_argument("-f", "--file", type=str, required=True, help="File which you want to unittests for")
+parser.add_argument("-d", "--directory", default="___default___", help="Location to write tests. Default is the target file's directory")
 parser.add_argument("-i", "--indented", action="store_true", help="Also looks for indented functions")
 parser.add_argument("-c", "--commented", action="store_true", help="Also looks for commented functions")
 
 args = parser.parse_args()
 
-instance = CreateTests(args.file, args.commented, args.indented)
+instance = CreateTests(args.file, args.directory, args.commented, args.indented)
 instance.write_tests()
 sys.stdout.write("{}\n".format(instance.userMessage))
