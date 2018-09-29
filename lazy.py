6fe6e7bb670905d9515c947780dab11d8f9e3d7a
connor-philip <connorphilip12@hotmail.com>
2018-09-30 00:07:09 +0100

    Formatting

--- lazy.py
+++ lazy.py
@@ -3,10 +3,14 @@ import argparse
 import sys
 
 parser = argparse.ArgumentParser(prog="command")
-parser.add_argument("-f", "--file", type=str, required=True, help="File which you want to unittests for")
-parser.add_argument("-d", "--directory", default="___default___", help="Location to write tests. Default is the target file's directory")
-parser.add_argument("-i", "--indented", action="store_true", help="Also looks for indented functions")
-parser.add_argument("-c", "--commented", action="store_true", help="Also looks for commented functions")
+parser.add_argument("-f", "--file", type=str, required=True,
+                    help="File which you want to unittests for")
+parser.add_argument("-d", "--directory", default="___default___",
+                    help="Location to write tests. Default is the target file's directory")
+parser.add_argument("-i", "--indented", action="store_true",
+                    help="Also looks for indented functions")
+parser.add_argument("-c", "--commented", action="store_true",
+                    help="Also looks for commented functions")
 
 args = parser.parse_args()
 
